4d4277a9837c05c6e16fa1c24c5503993b3a3684
CreeseOSuero <creeseandrenee.suero@smc.pshs.edu.ph>
2025-01-29 16:41:22 +0800

    Update Stask.js
    changed 2024 to 2025

--- QUARTER 3 - FA1/Stask.js
+++ QUARTER 3 - FA1/Stask.js
@@ -2,6 +2,6 @@ let firstname = prompt("Enter your first name : ")
 let lastname = prompt("Enter your last name : ")
 let birthyear = prompt("Enter your birth year : ")
 
-let age = 2024 - birthyear; 
+let age = 2025 - birthyear; 
 
 document.getElementById ("task2").innerText = "Hello " + firstname + " " + lastname + "!" + " How does it feel to be " + age + " years old?";
